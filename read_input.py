27c7ffae8364c4833e8d7ad9dc6784da73a9ea74
Sevy Harris <harris.se@northeastern.edu>
2021-09-28 17:34:53 -0400

    starting basic read

--- read_input.py
+++ read_input.py
@@ -1,5 +1,14 @@
 # script to read in the mechanism file
+from rmgpy.chemkin import load_chemkin_file
 
 
 def import_mechanism():
     print("reading mechanism")
+
+
+my_cti = '/home/moon/methanol/perturb_5000/run_0000/cantera/chem_annotated.cti'
+my_chem = '/home/moon/methanol/perturb_5000/run_0000/chemkin/chem_annotated-surface.inp'
+my_dict = '/home/moon/methanol/perturb_5000/run_0000/chemkin/species_dictionary.txt'
+
+species_list, reaction_list = load_chemkin_file(my_chem, dictionary_path=my_dict)
+print(species_list)
